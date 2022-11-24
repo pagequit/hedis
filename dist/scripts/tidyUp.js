2ae62d1696d39f58ef4d4e9f34317b60f4057cca
kolaflash <kolaflash@TREK>
2022-11-24 20:41:42 +0100

    WIP

--- dist/scripts/tidyUp.js
+++ dist/scripts/tidyUp.js
@@ -4,7 +4,7 @@ const node_fs_1 = require("node:fs");
 const redis_1 = require("redis");
 exports.default = (0, redis_1.defineScript)({
     NUMBER_OF_KEYS: 1,
-    SCRIPT: (0, node_fs_1.readFileSync)(__dirname + './../../lua/TIDYUP.lua', 'utf8'),
+    SCRIPT: (0, node_fs_1.readFileSync)(__dirname + '/../../lua/TIDYUP.lua', 'utf8'),
     transformArguments(key) {
         return [key];
     },
